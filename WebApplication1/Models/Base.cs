8866337abb14ba34ce7c22e70a19e58a1d298a2f
CM7CHAEL <cm7chael@gmail.com>
2023-09-27 09:15:49 -0500

    conection moongodb

---
+++ WebApplication1/Models/Base.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace wbserver.Models
+{
+    public class @Base
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("nombre")]
+        public string Nombre { get; set; }  
+
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using wbserver.Models;
 
 namespace WebApplication1
 {
@@ -13,6 +15,16 @@ namespace WebApplication1
     {
         public static void Main(string[] args)
         {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("prueba");
+
+            var pruebaDB = database.GetCollection<@Base>("base");
+
+            var baseObject = new Base() { Nombre = "Base 03" };
+            //pruebaDB.InsertOne(baseObject);  
+            //List<Base> lst = pruebaDB.Find(d => true).ToList();
+
+
             CreateHostBuilder(args).Build().Run();
         }
 
